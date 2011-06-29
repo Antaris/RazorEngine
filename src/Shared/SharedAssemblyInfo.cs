b7f7ab7424073169fe187ed1c9b432f90753807f
Matthew Abbott <matt@fidelitydesign.net>
2011-06-29 20:05:06 +0100

    Initial Core project commit

---
+++ src/Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Microsoft")]
+
+[assembly: AssemblyProduct("RazorEngine")]
+[assembly: AssemblyCopyright("Copyright © RazorEngine Project 2011")]
+
+[assembly: AssemblyVersion("3.0.0*")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
