b64ed7acf7d787f466d25edd5c27dcc3902b2bc6
Matthew Abbott <matt@fidelitydesign.net>
2012-01-31 22:43:53 +0000

    Stepped version number to 3.0.8beta

--- src/Shared/SharedAssemblyInfo.cs
+++ src/Shared/SharedAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("RazorEngine")]
 [assembly: AssemblyCopyright("Copyright © RazorEngine Project 2011")]
 
-[assembly: AssemblyVersion("3.0.7.0")]
-[assembly: AssemblyFileVersion("3.0.7.0")]
+[assembly: AssemblyVersion("3.0.8.0")]
+[assembly: AssemblyFileVersion("3.0.8.0")]
 
 //[assembly: CLSCompliant(true)]
