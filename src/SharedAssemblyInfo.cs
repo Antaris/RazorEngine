ad3e0cddbe9e00509a505b834970045ed94815a0
Matthias Dittrich <matthi.d@googlemail.com>
2015-01-23 19:52:31 +0100

    Bump version to 3.5.2

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("RazorEngine")]
 [assembly: AssemblyProductAttribute("RazorEngine")]
 [assembly: AssemblyCopyrightAttribute("Copyright � RazorEngine Project 2011-2014")]
-[assembly: AssemblyVersionAttribute("3.5.2.0")]
-[assembly: AssemblyFileVersionAttribute("3.5.2.0")]
+[assembly: AssemblyVersionAttribute("3.5.2")]
+[assembly: AssemblyFileVersionAttribute("3.5.2")]
 [assembly: AssemblyInformationalVersionAttribute("3.5.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.5.2.0";
+        internal const string Version = "3.5.2";
     }
 }
