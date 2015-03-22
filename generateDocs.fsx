41f64742e54974662f575133f2545fd013dbf271
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-22 20:43:13 +0100

    print assemblies and load assembly via path.

--- generateDocs.fsx
+++ generateDocs.fsx
@@ -7,18 +7,19 @@ open System.IO
 // As long as FSharp.Formatting is using the regular net45 build
 // This should work as expected.
 #I @"build/net45"
-#r "System.Web.Razor.dll"
-#r "RazorEngine.dll"
+#r "build/net45/System.Web.Razor.dll"
+#r "build/net45/RazorEngine.dll"
 
 #load "packages/Yaaf.AdvancedBuilding/content/buildConfigDef.fsx"
 #load @"buildConfig.fsx"
 #load "packages/Yaaf.AdvancedBuilding/content/generateDocsInclude.fsx"
 open Fake
-try
-  RunTargetOrDefault "LocalDoc"
-finally
-  printfn "Documentation generation finished. Loaded Assemblies:"
-  System.AppDomain.CurrentDomain.GetAssemblies()
-  |> Seq.choose (fun a -> try Some (a.GetName().FullName, a.Location) with _ -> None)
-  //|> Seq.filter (fun l -> l.Contains ("Razor"))
-  |> Seq.iter (fun (n, l) -> printfn "\t- %s: %s" n l)
+
+printfn "Documentation generation finished. Loaded Assemblies:"
+System.AppDomain.CurrentDomain.GetAssemblies()
+|> Seq.choose (fun a -> try Some (a.GetName().FullName, a.Location) with _ -> None)
+//|> Seq.filter (fun l -> l.Contains ("Razor"))
+|> Seq.iter (fun (n, l) -> printfn "\t- %s: %s" n l)
+
+RunTargetOrDefault "LocalDoc"
+
