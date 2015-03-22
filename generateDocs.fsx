6c60049c347334a614d2befad64364367cee3d8c
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-22 14:22:29 +0100

    print loaded assemblies when doc generation fails.

--- generateDocs.fsx
+++ generateDocs.fsx
@@ -14,4 +14,12 @@ open System.IO
 #load @"buildConfig.fsx"
 #load "packages/Yaaf.AdvancedBuilding/content/generateDocsInclude.fsx"
 open Fake
-RunTargetOrDefault "LocalDoc"
+try
+  RunTargetOrDefault "LocalDoc"
+with e ->
+  printfn "Exception in documentation generation. Loaded Assemblies:"
+  System.AppDomain.CurrentDomain.GetAssemblies()
+  |> Seq.choose (fun a -> try Some (a.GetName().FullName, a.Location) with _ -> None)
+  //|> Seq.filter (fun l -> l.Contains ("Razor"))
+  |> Seq.iter (fun (n, l) -> printfn "\t- %s: %s" n l)
+  reraise()
