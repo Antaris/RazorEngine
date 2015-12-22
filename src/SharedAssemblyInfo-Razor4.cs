24627780c65dd1b15627c218b440366af1f5733d
Matthias Dittrich <matthi.d@gmail.com>
2015-12-22 16:47:46 +0100

    Bump version to 3.7.6

--- src/SharedAssemblyInfo-Razor4.cs
+++ src/SharedAssemblyInfo-Razor4.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("RazorEngine")]
 [assembly: AssemblyProductAttribute("RazorEngine")]
 [assembly: AssemblyCopyrightAttribute("RazorEngine Copyright © RazorEngine Project 2011-2015")]
-[assembly: AssemblyVersionAttribute("4.2.5")]
-[assembly: AssemblyFileVersionAttribute("4.2.5")]
-[assembly: AssemblyInformationalVersionAttribute("4.2.5-beta1")]
+[assembly: AssemblyVersionAttribute("4.2.6")]
+[assembly: AssemblyFileVersionAttribute("4.2.6")]
+[assembly: AssemblyInformationalVersionAttribute("4.2.6-beta1")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "4.2.5";
+        internal const string Version = "4.2.6";
     }
 }
