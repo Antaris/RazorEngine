7200743857965259d163d5367e3e440a7ca4b2be
Matthias Dittrich <matthi.d@gmail.com>
2016-01-20 20:11:01 +0100

    Bump version to 3.7.7

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("RazorEngine")]
 [assembly: AssemblyProductAttribute("RazorEngine")]
 [assembly: AssemblyCopyrightAttribute("RazorEngine Copyright © RazorEngine Project 2011-2015")]
-[assembly: AssemblyVersionAttribute("3.7.6")]
-[assembly: AssemblyFileVersionAttribute("3.7.6")]
-[assembly: AssemblyInformationalVersionAttribute("3.7.6")]
+[assembly: AssemblyVersionAttribute("3.7.7")]
+[assembly: AssemblyFileVersionAttribute("3.7.7")]
+[assembly: AssemblyInformationalVersionAttribute("3.7.7")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "3.7.6";
+        internal const string Version = "3.7.7";
     }
 }
