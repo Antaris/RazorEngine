468947071fac93e4e3b7fcc562b3828b929bb015
Olivier Coanet <olivier.coanet@gmail.com>
2016-11-15 14:07:38 +0100

    Add TemplateRunner

---
+++ src/source/RazorEngine.Core/Templating/ITemplateRunnerOfTModel.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace RazorEngine.Templating
+{
+    /// <summary>
+    /// Defines the required contract for implementing a typed, runnable template reference.
+    /// </summary>
+    /// <typeparam name="TModel">The model type</typeparam>
+    public interface ITemplateRunner<TModel>
+    {
+        /// <summary>
+        /// Runs the template using the specified <paramref name="model"/>.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="textWriter"></param>
+        /// <param name="viewBag"></param>
+        void Run(TModel model, TextWriter textWriter, DynamicViewBag viewBag = null);
+    }
+}
